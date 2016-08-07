475ab27e560dd0fb5b6ac757c82c9267b932e652
Viktor Lazarev <taurus101v@gmail.com>
2016-08-08 02:06:32 +0400

    Add 'Reset' button to counter

--- counter.elm
+++ counter.elm
@@ -6,7 +6,7 @@ main: Program Never
 main =
   App.beginnerProgram { model = 0, view = view, update = update }
 
-type Msg = Increment | Decrement
+type Msg = Increment | Decrement | Reset
 
 update: Msg -> number -> number
 update msg model =
@@ -17,10 +17,14 @@ update msg model =
     Decrement ->
       model - 1
 
+    Reset ->
+      0
+
 view: a -> Html Msg
 view model =
   div []
     [ button [ onClick Decrement ] [ text "-" ]
     , div [] [ text (toString model) ]
     , button [ onClick Increment ] [ text "+" ]
+    , button [ onClick Reset ] [ text "Reset" ]
     ]
