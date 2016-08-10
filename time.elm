d252f0ceaf0258304dc5107c4f60763dfbd0e930
Viktor Lazarev <taurus101v@gmail.com>
2016-08-11 03:53:07 +0400

    Add minute & hour hands

--- time.elm
+++ time.elm
@@ -34,11 +34,21 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
   let
-    angle = turns (Time.inMinutes model)
-    handX = toString (50 + 40 * cos angle)
-    handY = toString (50 + 40 * sin angle)
+    angleSeconds = turns (Time.inMinutes model)
+    handYSeconds = toString (50 - 40 * cos angleSeconds)
+    handXSeconds = toString (50 + 40 * sin angleSeconds)
+
+    angleMinutes = angleSeconds / 60
+    handYMinutes = toString (50 - 37 * cos angleMinutes)
+    handXMinutes = toString (50 + 37 * sin angleMinutes)
+
+    angleHours = angleMinutes / 12 + turns 1/3
+    handYHours = toString (50 - 32 * cos angleHours)
+    handXHours = toString (50 + 32 * sin angleHours)
   in
     svg [ viewBox "0 0 100 100", width "300px" ]
       [ circle [ cx "50", cy "50", r"45", fill "#0b79ce" ] []
-      , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+      , line [ x1 "50", y1 "50", x2 handXSeconds, y2 handYSeconds, stroke "#023963" ] []
+      , line [ x1 "50", y1 "50", x2 handXMinutes, y2 handYMinutes, stroke "#023963", strokeWidth "2" ] []
+      , line [ x1 "50", y1 "50", x2 handXHours, y2 handYHours, stroke "#023963", strokeWidth "3" ] []
       ]
